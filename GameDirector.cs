137c705a9c23d38d3030a6260bb72b85cb1b70f5
kasukajima <zetta_minamimoto@softbank.ne.jp>
2021-11-24 15:01:20 +0900

    GameOver追加

--- GameDirector.cs
+++ GameDirector.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameDirector : MonoBehaviour
 {
     GameObject hpGauge;
+    int count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,10 @@ public class GameDirector : MonoBehaviour
     // Update is called once per frame
     public void DecreaseHp()
     {
-        this.hpGauge.GetComponent<Image>().fillAmount -=0.1f;
+        this.hpGauge.GetComponent<Image>().fillAmount -=0.2f;
+        this.count++;
+        if(this.count>=5){
+            SceneManager.LoadScene("GameOverScene");
+        }
     }
 }
